5b793a008c825db82c4906013e9446a50f19e71d
Tushar Sonawane <tusharlsonawane@gmail.com>
2017-05-01 20:55:26 +0530

    init challenge

---
+++ index.js
@@ -0,0 +1,32 @@
+/**
+ * This javascript file will constitute the entry point of your solution.
+ *
+ * Edit it as you need.  It currently contains things that you might find helpful to get started.
+ */
+
+// This is not really required, but means that changes to index.html will cause a reload.
+require('./site/index.html')
+// Apply the styles in style.css to the page.
+require('./site/style.css')
+
+// Change this to get detailed logging from the stomp library
+global.DEBUG = false
+
+const url = "ws://localhost:8011/stomp"
+const client = Stomp.client(url)
+client.debug = function(msg) {
+  if (global.DEBUG) {
+    console.info(msg)
+  }
+}
+
+function connectCallback() {
+  document.getElementById('stomp-status').innerHTML = "It has now successfully connected to a stomp server serving price updates for some foreign exchange currency pairs."
+}
+
+client.connect({}, connectCallback, function(error) {
+  alert(error.headers.message)
+})
+
+const exampleSparkline = document.getElementById('example-sparkline')
+Sparkline.draw(exampleSparkline, [1, 2, 3, 6, 8, 20, 2, 2, 4, 2, 3])
